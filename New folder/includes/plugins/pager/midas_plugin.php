15685dc9ccf2775329e836bf18d0290a8a964a13
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-01-03 01:39:39 +0530

    Delete New folder directory

--- New folder/includes/plugins/pager/midas_plugin.php
+++
@@ -1,3 +0,0 @@
-<?
-require(dirname(__FILE__)."/pager.class.php") ;
-require(dirname(__FILE__)."/pager_sql.class.php") ;
--- New folder/includes/plugins/pager/themes/1/displaying.inc.php
+++
@@ -1,1 +0,0 @@
-Showing Records: <?= $this->start+1?> to <?=min($this->total_records, $this->start+$this->records_per_page)?> of <?= $this->total_records?>
